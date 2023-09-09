5c5c90b0cbd53163125e73f86e70d20021383e8b
unknown <alamin.sheikh@sjinnovation.com>
2023-09-09 15:01:34 +0600

    errr

--- index.js
+++ index.js
@@ -33,17 +33,17 @@ app.use("/api/chatbot", chatbot)
 // const __dirname = path.resolve();
 // app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", async (req, res) => {
-    res.send("API is running....");
-  });
-}
+//   app.get("*", (req, res) =>
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+//   );
+// } else {
+//   app.get("/", async (req, res) => {
+//     res.send("API is running....");
+//   });
+// }
 
 app.use(notFound);
 app.use(errorHandler);
